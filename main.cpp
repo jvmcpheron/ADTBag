8d24603a0f9472efa4faa4fc0ae0724a24916884
Jane McPheron <jvmcpheron@dyn-10-140-246-187.d.umn.edu>
2023-04-10 13:04:14 -0500

    added baseline code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+Programming Project 7: ADT Bag
+Jane McPheron
+04/13/2023
+*/
+
+  
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+
+int main(){
+
+}
