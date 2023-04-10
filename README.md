12da54f2779da629af9455f9261606fe663d044b
Jane McPheron <jvmcpheron@dyn-10-140-246-187.d.umn.edu>
2023-04-10 13:24:11 -0500

    made the code funcitonal

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 People use bags all the time for a variety of different purposes. Bags are containers that hold things. In this assignment you will be creating the abstract data type (ADT) Bag. We will begin by identifying the fundamental characteristics of all bags. We will then try to identify the fundamental operations that you would expect to be able to carry out with a bag. From these bag attributes and operations we should then be able to write a class definition for the Bag ADT. 
 Details of the Bag ADT will be covered in class.
+
+##Must Include
+The Bag class definition
+A default constructor
+public member function add()
+public member function display()
--- main.cpp
+++ main.cpp
@@ -12,7 +12,36 @@ Jane McPheron
 #include <sstream>
 using namespace std;
 
+class Bag {
+    public:
+        Bag(){
+            vector<string> allStuff;
+        }
+
+        void display(){
+            for (int i = 0; i < allStuff.size(); i++) {
+                std::cout << allStuff[i] << ", ";
+            }
+        }
+        void add(string item){
+            allStuff.push_back(item);
+        }
+    private:
+        vector<string> allStuff;
+        
+};
 
 int main(){
+   Bag grabBag;
+   string item;
 
+   // Test add()
+   cout << "Enter an item ";
+   getline(cin, item);
+   while (item != "quit") {
+      grabBag.add(item);
+      cout << "Enter an item or 'quit': ";
+      getline(cin, item);
+   }
+   grabBag.display();
 }
